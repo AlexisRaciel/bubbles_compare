6fbe5da18284d4e771ba0890f8a3f0eca26b1969
AlexisRaciel <55038881+AlexisRaciel@users.noreply.github.com>
2024-11-27 16:43:05 -0600

    listo 3

--- import pandas as pd.py
+++ import pandas as pd.py
@@ -4,7 +4,7 @@ import xmltodict
 
 class Table:
     
-    # Constructor Methodxcs
+    # Constructor Methodxcsse
     def __init__(self, path_parts, path_bubbles):
         self.path_bubblesn = path_bubbles
         self.path_partsn = path_parts
